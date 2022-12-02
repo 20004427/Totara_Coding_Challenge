58508c449759ae2eaff6b336b3a802fae7a458a5
Matthew Bowers <bowers.matthew9@gmail.com>
2022-12-03 09:40:39 +1300

    Create StringFunctions.java

---
+++ src/main/java/StringFunctions.java
@@ -0,0 +1,18 @@
+public class StringFunctions {
+    private static StringFunctions instance;
+
+    private StringFunctions() {
+
+    }
+
+    public static StringFunctions getInstance(){
+        if (instance.equals(null)) {
+            instance = new StringFunctions();
+        }
+        return instance;
+    }
+
+    public static String wrap(String string, int length) {
+        return "";
+    }
+}
