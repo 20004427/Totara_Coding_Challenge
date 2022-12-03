7e65b4fe86330b720ee3f8636207c2ce87e5b571
Matthew Bowers <bowers.matthew9@gmail.com>
2022-12-03 22:22:21 +1300

    JUnit 5

--- src/test/java/com/Totara/Tests.java
+++ src/test/java/com/Totara/Tests.java
@@ -3,8 +3,15 @@ package com.Totara;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 public class Tests {
+	private static StringFunctions stringFunctions;
+	
+	@BeforeAll
+	public void init() {
+		stringFunctions = StringFunctions.getInstance();
+	}
 
 	@Test
 	public void test() {
