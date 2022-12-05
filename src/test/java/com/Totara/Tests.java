d1037c0235c904a721e5be460660019b46ccf36c
Matthew Bowers <bowers.matthew9@gmail.com>
2022-12-05 22:16:35 +1300

    Project setup fixes

--- src/test/java/com/Totara/Tests.java
+++ src/test/java/com/Totara/Tests.java
@@ -1,6 +1,6 @@
 package com.Totara;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeAll;
